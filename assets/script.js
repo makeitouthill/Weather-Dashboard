9e185375a310f7a6f54796ad7647e2fc6e117ca8
Alex Horning <67214695+makeitouthill@users.noreply.github.com>
2023-01-11 22:58:08 -0800

    Added OpenWeather and Opencage APIs to recieve latlong and weather for area, Added Fetch and Catch functions, Added a console log to log .catch errors

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,31 @@
 window.addEventListener('load', function(){
     var searchForm = document.getElementById("searchForm");
     var cityInput = document.getElementById("cityInput");
+    var API_KEY = "458ac284ed82506b853e5d277ec61cb1";
+    var city = cityInput.value;
+    var lat, lng;
     
-    function getWeather(){
-    console.log("getWeather")
+    function getWeather(lat, lng){
+        var API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lng}&appid=${API_KEY}`;
+        fetch(API_URL)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+            });
     }
     
     searchForm.addEventListener("submit", function(event){ 
         event.preventDefault();
+        city = cityInput.value;
         console.log("Submitted " + cityInput.value);
-        getWeather();
+        fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=5ffc6c893abd4262b33abf21d8deab53`)
+            .then(response => response.json())
+            .then(data => {
+                lat = data.results[0].geometry.lat;
+                lng = data.results[0].geometry.lng;
+                getWeather(lat, lng);
+            })
+            .catch(error => console.log(error));
         console.log("Weather fetched");
-    
     });
-    });
+});
